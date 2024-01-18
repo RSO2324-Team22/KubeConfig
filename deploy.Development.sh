40c1bde145d692051573bb36c3f78f03b311688e
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-01-18 02:46:08 +0100

    changes for a better tomorrow

--- deploy.Development.sh
+++ deploy.Development.sh
@@ -6,8 +6,7 @@ kubectl apply -f planning_service.yml
 kubectl apply -f planning_service_ingress.Development.yml
 kubectl apply -f attendance_service.yml
 kubectl apply -f attendance_service_ingress.Development.yml
-kubectl apply -f resource_service.yml
-kubectl apply -f resource_service_ingress.Development.yml
 kubectl apply -f kafka.yml
+kubectl apply -f graylog.yml
 kubectl apply -f webapp.yml
 kubectl apply -f webapp_ingress.Development.yml
