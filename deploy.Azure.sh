6d810604637516838edfac59bb280488792c060b
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-01-20 15:11:57 +0100

    serilog and ingress

--- deploy.Azure.sh
+++ deploy.Azure.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
+kubectl apply -f kafka.yml
+kubectl apply -f graylog.yml
 kubectl apply -f members_service.yml
 kubectl apply -f planning_service.yml
 kubectl apply -f attendance_service.yml
-kubectl apply -f kafka.yml
-kubectl apply -f graylog.yml
 kubectl apply -f webapp.yml
 kubectl apply -f webapp_ingress.Azure.yml
--- deploy.Development.sh
+++ deploy.Development.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+kubectl apply -f kafka.yml
+kubectl apply -f graylog.yml
 kubectl apply -f members_service.yml
 kubectl apply -f members_service_ingress.Development.yml
 kubectl apply -f planning_service.yml
 kubectl apply -f planning_service_ingress.Development.yml
 kubectl apply -f attendance_service.yml
 kubectl apply -f attendance_service_ingress.Development.yml
-kubectl apply -f kafka.yml
-kubectl apply -f graylog.yml
 kubectl apply -f webapp.yml
 kubectl apply -f webapp_ingress.Development.yml
