395335f2cadba8563e5c1af297d1fdb7d2bbde7a
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-01-22 11:18:36 +0100

    making this work

--- deploy_azure.sh
+++ deploy_azure.sh
@@ -6,4 +6,8 @@ kubectl apply -f members_service.yml
 kubectl apply -f planning_service.yml
 kubectl apply -f attendance_service.yml
 kubectl apply -f webapp.yml
-kubectl apply -f webapp_ingress.Azure.yml
+kubectl apply -f ingress_azure.yml
+
+kubectl apply -f monitoring_graylog.yml
+kubectl apply -f monitoring_prometheus.yml
+kubectl apply -f monitoring_ingress_azure.yml
---
+++ deploy_development.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+kubectl apply -f kafka.yml
+kubectl apply -f graylog.yml
+kubectl apply -f members_service.yml
+kubectl apply -f planning_service.yml
+kubectl apply -f attendance_service.yml
+kubectl apply -f webapp.yml
+kubectl apply -f ingress_development.yml
+
+kubectl create namespace monitoring
+kubectl apply -f monitoring_graylog.yml
+kubectl apply -f monitoring_prometheus.yml
+kubectl apply -f monitoring_ingress_development.yml
+
