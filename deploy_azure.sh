8442dec495c0f8c873c2369e9d20828d388c420c
Jure Pustoslemšek <jure.poglednik@gmail.com>
2024-01-25 02:34:29 +0100

    azure

--- deploy_azure.sh
+++ deploy_azure.sh
@@ -9,6 +9,8 @@ kubectl apply -f attendance_service.yml
 kubectl apply -f webapp.yml
 kubectl apply -f ingress_azure.yml
 
+kubectl create namespace monitoring
 kubectl apply -f monitoring_graylog.yml
 kubectl apply -f monitoring_prometheus.yml
 kubectl apply -f monitoring_ingress_azure.yml
+
